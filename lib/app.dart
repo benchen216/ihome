eeb79fcaa0de105aee24770a1d93536988bd2d1e
benchen <benchen216@gmail.com>
2018-11-20 21:28:57 +0800

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+
+class App extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AppState();
+  }
+
+}
+class AppState extends State<App>{
+  int counter = 0;
+
+  void fetchImage () async {
+    counter++;
+    var response = await get('https://jsonplacehoder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('$counter'),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
+        appBar: AppBar(
+          backgroundColor: Color(0xFF673AB7),
+          title: Text("here"),
+        ),
+      ),
+    );
+  }
+
+
+
+}
