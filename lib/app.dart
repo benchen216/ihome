3a7f9dcf50b000cbd1f76139f4a649f12d4c1dca
ben <benchen216@gmail.com>
2018-11-21 02:14:09 +0800

    完成課程範例

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 import 'models/image_model.dart';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget{
   @override
@@ -16,16 +17,18 @@ class AppState extends State<App>{
   List<ImageModel> images = [];
   void fetchImage () async {
     counter++;
-    var response = await get('https://jsonplacehoder.typicode.com/photos/$counter');
+    var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
     var imageModel = ImageModel.fromJson(json.decode(response.body));
-    images.add(imageModel);
+    setState(() {
+      images.add(imageModel);
+    });
   }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-        body: Text('$counter'),
+        body: ImageList(images),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: fetchImage,
